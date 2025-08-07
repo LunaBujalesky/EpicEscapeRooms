de80f667a310fe403c84e972d26ab837ec234ae7
Luna Bujalesky <lunitabruja@gmail.com>
2025-08-07 03:31:52 -0300

    añadidos basicos

--- components.js
+++ components.js
@@ -3,3 +3,4 @@ document.getElementById("testimonios-searchbar-puzzle").addEventListener("focus"
   alert(" *No hace nada, /n" +
     "Que esperabas de una barra de búsqueda que está en medio de la nada? /n pero, ey! es un testimonio de que la barra de búsqueda está funcionando.  ");
 });
+
