d11d699c6fde1541b7e1e95a28f80142ba181094
Luna Bujalesky <lunitabruja@gmail.com>
2025-08-26 23:48:48 -0300

    bugs js corregidos

--- components.js
+++ components.js
@@ -1,9 +1,4 @@
 //botón de scroll para enfatizar la presentación -------------------------------------------------------------------------
-document.addEventListener("DOMContentLoaded", () => {
-  EventoBarraBusqueda();
-  EventoBotonEscalera();
-  
-});
 
 function EventoBarraBusqueda() {
   const barraBusqueda = document.getElementById("testimonios-searchbar-puzzle");
@@ -22,7 +17,7 @@ function EventoBarraBusqueda() {
 function EventoBotonEscalera() {
   const botonEscalera = document.getElementById("boton-escalera");
   const escaleraSeccion = document.getElementById("escalera-seccion");
-  if (!escaleraSeccion || !botonEscalera ) {
+  if (!escaleraSeccion || !botonEscalera) {
     return;
   }
   botonEscalera.addEventListener("click", () => {
@@ -31,41 +26,85 @@ function EventoBotonEscalera() {
 }
 
 //Sistema de partículas, libreria externa: https://github.com/VincentGarreau/particles.js
-//el de inicio
-particlesJS.load('particles-js',   'data.json/particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
 
-//el de sala secreta
-particlesJS.load('particles2',  '../data.json/particlesjs2sala.json',  function() {
-  console.log('callback - particles.js config loaded');
-});
+function cargarParticulasIndex() {
+  //el de inicio
+  const particulasIndex = document.getElementById("particles-js");
+  if (!particulasIndex) {
+    return;
+  }
+  particlesJS.load('particles-js', 'data.json/particles.json', function () {
+    console.log('callback - particles.js config loaded');
+
+  });
+}
+
+function cargarParticulasSalaSecreta() {
+  //el de sala secreta
+  const particulasSalaSecreta = document.getElementById("particles2");
+  if (!particulasSalaSecreta) {
+    return;
+  }
+  particlesJS.load('particles2', '../data.json/particlesjs2sala.json', function () {
+    console.log('callback - particles.js config loaded');
+
+  });
+}
 
 //animación de contacto que debe ejecutarse solo una vez por sesión ------------------------------
-window.addEventListener("load",() => {
- 
-  const papelito = document.querySelector(".papel-contacto-container");
-  setTimeout(() => { papelito.classList.add("papel-contacto-animacion");} , 1000)
-});
-  // Chequear si ya se animó esta sesión
-  //if (!sessionStorage.getItem("animacionDeslizada")) {
-   // papelito.classList.add("papel-contacto-animacion");
-    //sessionStorage.setItem("animacionDeslizada", "true");
-  //} else {
-    //estado final
-    //papelito.style.transform = "translateY(216px)";
-  
-  //}
 
+window.addEventListener("load", () => {
+  const papelito = document.querySelector(".papel-contacto-container");
+  if (!papelito) {
+    return;
+  }
+  setTimeout(() => { papelito.classList.add("papel-contacto-animacion"); }, 1000)
+});
+
+
+// Chequear si ya se animó esta sesión
+//if (!sessionStorage.getItem("animacionDeslizada")) {
+// papelito.classList.add("papel-contacto-animacion");
+//sessionStorage.setItem("animacionDeslizada", "true");
+//} else {
+//estado final
+//papelito.style.transform = "translateY(216px)";
+
+//}
+
+function spawnearMapa() {
   //toggle para mostrar la ubicación en el mapa
+  const papeles = document.querySelectorAll(".papel-contacto-container");
+  const mapa = document.querySelector("mapa-contacto");
+  if (!papeles) {
+    return;
+  }
   papeles.forEach(papel => {
     papel.addEventListener("click", () => {
       mapa.classList.toggle("ocultar-mapa");
     });
   });
+}
 
-  //api vainilla calendar para reservas de salas ---------------------------------------------------
-  document.addEventListener('DOMContentLoaded', () => {
-    const calendar = new VanillaCalendar('#calendar');
-    calendar.init();
-  });
+//api vainilla calendar para reservas de salas ---------------------------------------------------
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const elementoCalendar = document.getElementById('calendar');
+  if (!elementoCalendar) {
+    return;
+  }
+  const calendar = new VanillaCalendar('#calendar'); //new:objetonuevo, #id del objeto
+  calendar.init();
+});
+
+
+
+//ejecución de funciones -------------------------------------------------------------------------
+EventoBarraBusqueda();
+EventoBotonEscalera();
+cargarParticulasIndex();
+cargarParticulasSalaSecreta();
+
+spawnearMapa();
+
