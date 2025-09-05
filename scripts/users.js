32995b5701c3475d416c9eb5b9f9739f357cb8f2
Luna Bujalesky <lunitabruja@gmail.com>
2025-09-04 22:43:37 -0300

    x

--- scripts/users.js
+++ scripts/users.js
@@ -3,65 +3,65 @@ let contrasena;
 let usuarioActual;
 
 let usuarios = [
-    {
-      nombre: "Luna",
-      email: "lunitabruja@gmail.com",
-      contrasena: "1234",
-      fechaNacimiento: "1997-10-10"
-    },
-    {
-      nombre: "Pipo",
-      email: "pipo@gmail.com",
-      contrasena: "abcd",
-      fechaNacimiento: "2025-12-20"
-    }
-  ];
+  {
+    nombre: "Luna",
+    email: "lunitabruja@gmail.com",
+    contrasena: "1234",
+    fechaNacimiento: "1997-10-10"
+  },
+  {
+    nombre: "Pipo",
+    email: "pipo@gmail.com",
+    contrasena: "abcd",
+    fechaNacimiento: "2025-12-20"
+  }
+];
 // Funcion crear Usuario por html y dom
 function registrarUsuario() {
-    const formRegistro = document.getElementById("formRegistro")
-    if (!formRegistro) {
-        return
+  const formRegistro = document.getElementById("formRegistro")
+  if (!formRegistro) {
+    return
+  }
+  formRegistro.addEventListener("submit", function (event) {
+    event.preventDefault(); // Evita que recargue la página
+
+    let usuarios = JSON.parse(localStorage.getItem("usuarios"));
+
+    let nombre = document.getElementById("nombre").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let contrasena = document.getElementById("contrasena").value.trim();
+    let fechaNacimiento = document.getElementById("fechaNacimiento").value.trim();
+
+    let usuario = {
+      nombre: nombre,
+      email: email,
+      contrasena: contrasena,
+      fechaNacimiento: fechaNacimiento,
+    };
+
+    // chequear longitud de la contraseña/* -->
+    if (contrasena.length < 4) {
+      alert("Contraseña inválida. Por favor, genere una nueva.");
+      return registrarUsuario();
     }
-    formRegistro.addEventListener("submit", function (event) {
-        event.preventDefault(); // Evita que recargue la página
 
-        let usuarios = JSON.parse(localStorage.getItem("usuarios"));
+    //chequear que el mail esté registrado en el array
+    usuarioActual = usuarios.find((usuario) => usuario.email == email);
 
-        let nombre = document.getElementById("nombre").value.trim();
-        let email = document.getElementById("email").value.trim();
-        let contrasena = document.getElementById("contrasena").value.trim();
-        let fechaNacimiento = document.getElementById("fechaNacimiento").value.trim();
+    if (usuarioActual) {
+      alert("Ese mail ya fue registrado");
+      return;
+    }
 
-        let usuario = {
-            nombre: nombre,
-            email: email,
-            contrasena: contrasena,
-            fechaNacimiento: fechaNacimiento,
-        };
+    usuarios.push(usuario);
 
-        // chequear longitud de la contraseña/* -->
-        if (contrasena.length < 4) {
-            alert("Contraseña inválida. Por favor, genere una nueva.");
-            return registrarUsuario();
-        }
-
-        //chequear que el mail esté registrado en el array
-        usuarioActual = usuarios.find((usuario) => usuario.email == email);
-
-        if (usuarioActual) {
-            alert("Ese mail ya fue registrado");
-            return;
-        }
-
-        usuarios.push(usuario);
-
-        // Guardar nuevo objeto en el array ...................................-->
-        //localStorage.setItem(usuario);
-        localStorage.setItem("usuarios", JSON.stringify(usuarios));
+    // Guardar nuevo objeto en el array ...................................-->
+    //localStorage.setItem(usuario);
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
 
 
-        window.open("logIn.html", "_self");
-    })
+    window.open("logIn.html", "_self");
+  })
 };
 
 
@@ -73,40 +73,47 @@ function registrarUsuario() {
 
 function VerificarDatos() {
 
-    const formLogin = document.getElementById("formLogin");
+  const formLogin = document.getElementById("formLogin");
 
-    if (!formLogin) { return }
+  if (!formLogin) { return }
 
-    formLogin.addEventListener("submit", function (event) {
+  formLogin.addEventListener("submit", function (event) {
 
+    event.preventDefault(); // Evita que recargue la página
 
-        event.preventDefault(); // Evita que recargue la página
+    email = document.getElementById("email").value.trim();
+    contrasena = document.getElementById("contrasena").value.trim();
 
-        email = document.getElementById("email").value;
-        contrasena = document.getElementById("contrasena").value;
-
-        //chequear que el mail esté registrado en el array
-        usuarioActual = usuarios.find((usuario) => usuario.email == email);
-
-        if (!usuarioActual) {
-            alert("El email no está registrado.");
-            return;
-        }
-        //chequear que la contraseña coincida con la del array
-        if (usuarioActual.contrasena !== contrasena) {
-            alert("Contraseña incorrecta.");
-            return;
-        }
-
-        //Redirije si todo conside:
-        if (usuarioActual.contrasena === contrasena) {
-            // Guardar su nombre para saludarlo en su perfil
-            localStorage.setItem("nombreUsuario", usuarioActual.nombre);
-            window.open("perfil.html", "_self");
-        }
+    console.log(contrasena + "che que onda");
+    console.log(email + "che que onda");
 
 
-    })
+
+    //chequear que el mail esté registrado en el array
+    usuarioActual = usuarios.find((usuario) => usuario.email == email);
+
+    localStorage.setItem("nombreUsuario", usuarioActual.nombre);
+
+    if (!usuarioActual) {
+      alert("El email no está registrado.");
+      return;
+    }
+
+    //chequear que la contraseña coincida con la del array
+    if (usuarioActual.contrasena !== contrasena) {
+      alert("Contraseña incorrecta.");
+      return;
+    }
+
+    //Redirije si todo conside:
+    if (usuarioActual.contrasena === contrasena) {
+      // Guardar su nombre para saludarlo en su perfil
+      localStorage.setItem("nombreUsuario", usuarioActual.nombre);
+      window.location.href = "perfil.html";
+    }
+
+
+  })
 };
 
 
@@ -114,21 +121,21 @@ function VerificarDatos() {
 
 
 function redireccionLogin() {
-    let menuLogin = document.getElementById("redireccionporLogin")
-    let menuSalaSecreta = document.getElementById("redireccionSalaSecreta")
-    if (!menuLogin || !menuSalaSecreta) {return}
-    //usuario ingresado
-    if (usuarioActual) {
-        menuLogin.href =  "pages/perfil.html";
-        menuSalaSecreta.href = "pages/salaabierta.html";
-    }
+  let menuLogin = document.getElementById("redireccionporLogin")
+  let menuSalaSecreta = document.getElementById("redireccionSalaSecreta")
+  if (!menuLogin || !menuSalaSecreta) { return }
+  //usuario ingresado
+  if (usuarioActual) {
+    menuLogin.href = "pages/perfil.html";
+    menuSalaSecreta.href = "pages/salaabierta.html";
+  }
 
-    //por default, usuario POR ingresar 
-    else {
-        menuLogin.href =  "pages/logIn.html";
-        menuSalaSecreta.href = "pages/salaSecreta.html";
-    }
-} 
+  //por default, usuario POR ingresar 
+  else {
+    menuLogin.href = "pages/logIn.html";
+    menuSalaSecreta.href = "pages/salaSecreta.html";
+  }
+}
 
 
 // Funcion para Saludar al usuario segun el momento del dia con condicional switch 
@@ -136,37 +143,37 @@ function redireccionLogin() {
 // if lo usamos para verificar si hay un nombre registrado. 
 // switch lo usamos para decidir el saludo según la hora del día. /* -->
 function obtenerMomentoDelDia() {
-    const hora = new Date().getHours();
-  
-    if (hora >= 6 && hora < 12) return "mañana";
-    if (hora >= 12 && hora < 20) return "tarde";
-    return "noche";
-  }
-  
+  const hora = new Date().getHours();
+
+  if (hora >= 6 && hora < 12) return "mañana";
+  if (hora >= 12 && hora < 20) return "tarde";
+  return "noche";
+}
+
 
 function saludarUsuario() {
-    const nombre = localStorage.getItem("nombre");
-    if (!nombre) return; 
-  
-    const momentoDelDia = obtenerMomentoDelDia();
-  
-    let saludo;
-    switch (momentoDelDia) {
-      case "mañana":
-        saludo = "Buenos días";
-        break;
-      case "tarde":
-        saludo = "Buenas tardes";
-        break;
-      case "noche":
-        saludo = "Buenas noches";
-        break;
-      default:
-        saludo = "Hola";
-    }
-  
-    saludoUsuario.textContent = `${saludo}, ${nombre}! Qué bueno verte otra vez :) `;
+  const nombre = localStorage.getItem("nombre");
+  if (!nombre) return;
+
+  const momentoDelDia = obtenerMomentoDelDia();
+
+  let saludo;
+  switch (momentoDelDia) {
+    case "mañana":
+      saludo = "Buenos días";
+      break;
+    case "tarde":
+      saludo = "Buenas tardes";
+      break;
+    case "noche":
+      saludo = "Buenas noches";
+      break;
+    default:
+      saludo = "Hola";
   }
+
+  saludoUsuario.textContent = `${saludo}, ${nombre}! Qué bueno verte otra vez :) `;
+}
 
 // FIN  Funcion  Saludar/* ...................................-->
 
